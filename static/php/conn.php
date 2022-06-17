a93142231c1cae5e499bf2cf457c7c80dfabbf8e
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2022-06-17 14:31:45 +0800

    chart, update link

---
+++ static/php/conn.php
@@ -0,0 +1,11 @@
+<?php 
+    $host_name = "127.0.0.1";
+    $database = "db_name";
+    $user_name = "root";
+    $password = "";
+
+    function get_db_connection() {
+        global $host_name, $database, $user_name, $password;
+        return mysqli_connect( $host_name, $user_name, $password, $database );
+    };
+?>
