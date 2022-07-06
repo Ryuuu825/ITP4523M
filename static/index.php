b9e7620a81f7a3fe1a3e3487ce5c464ce8c22bd1
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2022-07-06 11:29:38 +0800

    add login, and redirect to login page when user no login

--- static/index.php
+++ static/index.php
@@ -18,23 +18,56 @@ require_once("./php/http_helper.php");
 require_once("./php/conn.php");
 
 $conn;
+
+try 
+{
+    $conn = get_db_connection();
+}catch(Exception)
+{   
+    internal_server_error("Could not connect to database." , "./pages/500.php");
+}
+
 if ($is_try_login)
 {
     $username = $_POST["UserName"];
     $password = $_POST["Pwd"];
+
+    $sql = "SELECT * FROM `Staff` WHERE `staffID` = '$username'";  // SQL query to get the user's information
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+
+    // check if the user exists
+    if ($row)
+    {
+        // check if the password is correct
+        if ($row["password"] == $password)
+        {
+            session_start();
+
+            // create a cookie to remember the user's login
+            // and hashed username
+            $_SESSION["username"] =  hash('sha256' , $username);
+            var_dump($_SESSION["username"]);
+
+            // redirect to the homepage
+            header("Location: ./pages/template.php");
+
+            exit;
+        }
+
+        $isLoginOk = false;
+    }
+    else
+    {
+        $isLoginOk = false;
+    }
 }
 
 
 ?>
 <!-- <?php 
 
-        try 
-        {
-            $conn = get_db_connection();
-        }catch(Exception)
-        {   
-            internal_server_error("Could not connect to database." , "./pages/500.php");
-        }
+        
     ?> -->
     <div class="d-flex align-items-center flex-column justify-content-center" style="height:100vh">
             <div class="d-block bg-light border rounded shadow p-1 mb-3" style="width:450px">
@@ -46,14 +79,14 @@ if ($is_try_login)
                 </div>
                 <form action="<?php $_SERVER['PHP_SELF'] ?>" method="Post" class="p-5">
                     <label for="UserName" class="form-label">User Name</label>
-                    <input type="text" required class="rounded-pill form-control border-gray " style="padding: 0.5rem 1rem 0.5rem 1rem;" id="UserName" name="UserName" >
+                    <input type="text" required class="rounded-pill form-control border-gray <?php if (isset($isLoginOk)) echo 'is-invalid'; ?>" style="padding: 0.5rem 1rem 0.5rem 1rem;" id="UserName" name="UserName" >
                     <div class="invalid-feedback">
-                        No user found
+                        Incorrect
                     </div>
                     <label for="Password" class="form-label mt-3">Password</label>
-                    <input type="password" required class="form-control rounded-pill border-gray" style="padding: 0.5rem 1rem 0.5rem 1rem;" id="Password" name="Pwd">
+                    <input type="password" required class="form-control rounded-pill border-gray <?php if (isset($isLoginOk)) echo 'is-invalid'; ?> " style="padding: 0.5rem 1rem 0.5rem 1rem;" id="Password" name="Pwd">
                     <div class="invalid-feedback">
-                        Your password is incorrect.
+                    Incorrect
                     </div>
                     <button type="submit" class="btn btn-primary w-100 text-center mt-5 text-light shadow p-2 mb-3 rounded">Login</button>
                     <div class="text-info pe-auto">
