97a5f3fa778a842879604d24a8223ddfa6fab93d
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2022-07-08 17:45:45 +0800

    add back ben changes

--- static/pages/account.php
+++ static/pages/account.php
@@ -62,7 +62,7 @@
                 <th scope="col">#</th>
                 <th scope="col">Name</th>
                 <th scope="col">Email</th>
-                <th scope="col">Number</th>
+                <th scope="col">Phone</th>
                 <?php 
                   if($_SESSION["position"] == "Manager")
                   {
