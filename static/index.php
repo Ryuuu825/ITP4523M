0ce0da96a4b6b7be82cbb4d6b8518db949332507
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2022-07-10 11:51:31 +0800

    real re-direct user to login pages/home pages

--- static/index.php
+++ static/index.php
@@ -1,4 +1,10 @@
 <?php 
-    header("./index.php");
-    exit;
+    // re-direct to the login page if the user is not logged in
+    if (!isset($_SESSION['username'])) {
+        header('Location: login.php');
+    }
+    else 
+    {
+        header("Location: ./pages/index.php");
+    }
 ?>
